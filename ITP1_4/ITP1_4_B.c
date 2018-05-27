c2e60483c0b87210b74dbd002884af9bef82e773
kisamasaki <31547716+kisamasaki@users.noreply.github.com>
2018-05-27 11:56:48 +0900

    Update and rename ITP1_4_B.c to ITP1_4_B.cpp

--- ITP1_4/ITP1_4_B.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    double r;
-    scanf("%lf",&r);
-    printf("%lf %lf",r*r*M_PI,2*r*M_PI);
-}
---
+++ ITP1_4/ITP1_4_B.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+using namespace std;
+
+int main(){
+    double r;
+    cin >> r;
+    printf("%lf %lf",r*r*M_PI,2*r*M_PI);
+    return 0;
+}
