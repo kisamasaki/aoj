a12ae4ad9a1afaca86c66857d0269e33b04b530b
kisamasaki <31547716+kisamasaki@users.noreply.github.com>
2018-08-22 21:23:32 +0900

    Add files via upload

---
+++ ITP1_8/ITP1_8_A.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>                                         
+#include<cctype>
+#include<stdio.h>                                  
+using namespace std;
+                                                   
+int main(){
+  char ch;                                                                                   
+  while(1){
+  scanf("%c",&ch);
+    if(islower(ch)){
+      printf("%c",toupper(ch));
+    } else {
+      printf("%c",tolower(ch));
+    }                                 
+    if(ch == '\n') break;
+  }
+}
