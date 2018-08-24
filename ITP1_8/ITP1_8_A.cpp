0756866dba23cf1052892cd8c6468526220b751c
kisamasaki <31547716+kisamasaki@users.noreply.github.com>
2018-08-24 12:50:37 +0900

    Add files via upload

--- ITP1_8/ITP1_8_A.cpp
+++ ITP1_8/ITP1_8_A.cpp
@@ -1,17 +1,14 @@
-#include<cstdio>                                         
-#include<cctype>
-#include<stdio.h>                                  
+#include <iostream>
 using namespace std;
-                                                   
-int main(){
-  char ch;                                                                                   
-  while(1){
-  scanf("%c",&ch);
-    if(islower(ch)){
-      printf("%c",toupper(ch));
-    } else {
-      printf("%c",tolower(ch));
-    }                                 
-    if(ch == '\n') break;
-  }
+
+int main () {
+	string str;
+	int n[26]={0};
+	while (getline(cin,str)) {
+		for (int i=0; i<str.length(); i++) {
+			if (str[i]>='A'&&'Z'>=str[i]) n[str[i]-'A']++;
+			else if (str[i]>='a'&&'z'>=str[i]) n[str[i]-'a']++;
+		}
+	}
+	for (int i=0; i<26; i++) cout << ((char)('a'+i)) << " : " << n[i] << endl;
 }
