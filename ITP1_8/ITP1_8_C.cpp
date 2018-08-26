7ea3200b9a3654c57f460a2970de2eba25f7a9cc
kisamasaki <31547716+kisamasaki@users.noreply.github.com>
2018-08-26 22:08:10 +0900

    Add files via upload

---
+++ ITP1_8/ITP1_8_C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+    string str;
+    int n[26]={};
+    while (getline(cin,str)) {
+        for (int i=0; i<str.length(); i++) {
+            if (str[i]>='A'&&'Z'>=str[i]) n[str[i]-'A']++;
+            else if (str[i]>='a'&&'z'>=str[i]) n[str[i]-'a']++;
+        }
+    }
+    for (int i=0; i<26; i++) cout << ((char)('a'+i)) << " : " << n[i] << endl;
+}
